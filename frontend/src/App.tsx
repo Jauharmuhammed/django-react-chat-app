b271efd5dd323528fe42766e6f9eb5d56d1eea5d
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-07 17:27:26 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Chat } from "./components/Chat";
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <AuthContextProvider>
+                            <Navbar />
+                        </AuthContextProvider>
+                    }>
+                    <Route path="" element={<Chat />} />
+                    <Route path="login" element={<Login />} />
+                    {/* <Route
+                        path=""
+                        element={
+                            <ProtectedRoute>
+                            <Conversations />
+                            </ProtectedRoute>
+                        }
+                        /> */}
+                    <Route
+                        path="chats/:conversationName"
+                        element={
+                            <ProtectedRoute>
+                                <Chat />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,98 @@
+import React, { useContext, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { AuthContext } from "../contexts/AuthContext";
+
+export function Chat() {
+    const { user } = useContext(AuthContext);
+
+    const [welcomeMessage, setWelcomeMessage] = useState("");
+    const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+
+    const [messageHistory, setMessageHistory] = useState<any>([]);
+
+    function handleChangeMessage(e: any) {
+        setMessage(e.target.value);
+    }
+
+    function handleChangeName(e: any) {
+        setName(e.target.value);
+    }
+
+    function handleSubmit() {
+        sendJsonMessage({
+            type: "chat_message",
+            message,
+            name,
+        });
+        setName("");
+        setMessage("");
+    }
+
+    const { sendJsonMessage } = useWebSocket("ws://127.0.0.1:8000/");
+
+    const { readyState } = useWebSocket(user ? "ws://127.0.0.1:8000/" : null, {
+        queryParams: {
+          token: user ? user.token : "",
+        },
+        onOpen: () => {
+            console.log("Connected!");
+        },
+        onClose: () => {
+            console.log("Disconnected!");
+        },
+        // New onMessage handler
+        onMessage: (e) => {
+            const data = JSON.parse(e.data);
+            switch (data.type) {
+                case "welcome_message":
+                    setWelcomeMessage(data.message);
+                    break;
+                case "chat_message_echo":
+                    setMessageHistory((prev: any) => prev.concat(data));
+                    break;
+                default:
+                    console.error("Unknown message type!");
+                    break;
+            }
+        },
+    });
+
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: "Connecting",
+        [ReadyState.OPEN]: "Open",
+        [ReadyState.CLOSING]: "Closing",
+        [ReadyState.CLOSED]: "Closed",
+        [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+    }[readyState];
+
+    return (
+        <div className=" flex flex-col w-full justify-center min-h-screen items-center gap-5">
+            <input
+                name="name"
+                placeholder="Name"
+                onChange={handleChangeName}
+                value={name}
+                className="px-4 py-2 shadow-sm sm:text-sm border-gray-300 bg-gray-100 rounded-md"
+            />
+            <input
+                name="message"
+                placeholder="Message"
+                onChange={handleChangeMessage}
+                value={message}
+                className="px-4 py-2  shadow-sm sm:text-sm border-gray-300 bg-gray-100 rounded-md"
+            />
+            <button className="bg-gray-300 px-3 py-1 " onClick={handleSubmit}>
+                Submit
+            </button>
+            <hr />
+            <ul>
+                {messageHistory.map((message: any, idx: number) => (
+                    <div className="border border-gray-200 py-3 px-3" key={idx}>
+                        {message.name}: {message.message}
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+}
